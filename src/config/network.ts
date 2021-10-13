b1173be3cf3c9492a215e9dc30a6e3bd35843b56
sxlwar <sxlwar@163.com>
2021-10-13 14:09:59 +0800

    chore: update chain name

--- src/config/network.ts
+++ src/config/network.ts
@@ -7,101 +7,113 @@ export enum Network {
 }
 
 export const NETWORK_CONFIG: NetworkConfig = {
+  crab: {
+    api: {
+      subql: 'https://api.subquery.network/sq/darwinia-network/crab',
+    },
+    dvmWithdrawAddress: {
+      kton: '0x0000000000000000000000000000000000000016',
+      ring: '0x0000000000000000000000000000000000000015',
+    },
+    erc20: {
+      kton: '0xbfE9E136270cE46A2A6a8E8D54718BdAEBEbaA3D',
+      ring: '0x588abe3F7EE935137102C5e2B8042788935f4CB0',
+    },
+    ethereumChain: {
+      blockExplorerUrls: ['https://crab.subscan.io/'],
+      chainId: '0x2c',
+      chainName: 'crab',
+      iconUrls: [],
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Crab Network Native Token',
+        symbol: 'CRAB',
+      },
+      rpcUrls: ['https://crab-rpc.darwinia.network/'],
+    },
+    facade: {
+      logo: '/image/crab.svg',
+      logoWithText: '/image/crab-logo.svg',
+    },
+    fullName: 'Crab',
+    rpc: 'wss://crab-rpc.darwinia.network',
+    ss58Prefix: 42,
+    token: {
+      kton: 'CKTON',
+      ring: 'CRAB',
+    },
+  },
   darwinia: {
+    api: {
+      subql: 'https://api.subquery.network/sq/darwinia-network/darwinia',
+    },
+    dvmWithdrawAddress: {
+      kton: '',
+      ring: '',
+    },
+    erc20: {
+      kton: '',
+      ring: '',
+    },
+    ethereumChain: {
+      blockExplorerUrls: [''],
+      chainId: '',
+      chainName: 'darwinia',
+      iconUrls: [],
+      nativeCurrency: {
+        decimals: 18,
+        name: '',
+        symbol: '',
+      },
+      rpcUrls: [],
+    },
     // TODO
     facade: {
       logo: '/image/darwinia.svg',
       logoWithText: '/image/darwinia-logo.svg',
     },
-    fullName: 'Darwinia Mainnet',
-    ss58Prefix: 18,
-    dvmWithdrawAddress: { ring: '', kton: '' },
-    token: { ring: 'RING', kton: 'KTON' },
-    erc20: {
-      ring: '',
-      kton: '',
-    },
+    fullName: 'Darwinia',
     rpc: 'wss://rpc.darwinia.network',
-    ethereumChain: {
-      chainId: '',
-      chainName: 'darwinia',
-      iconUrls: [],
-      nativeCurrency: {
-        name: '',
-        symbol: '',
-        decimals: 18,
-      },
-      rpcUrls: [],
-      blockExplorerUrls: [''],
-    },
-    api: {
-      subql: 'https://api.subquery.network/sq/darwinia-network/darwinia',
+    ss58Prefix: 18,
+    token: {
+      kton: 'KTON',
+      ring: 'RING',
     },
   },
   pangolin: {
-    facade: {
-      logo: '/image/pangolin.svg',
-      logoWithText: '/image/pangolin-logo.svg',
+    api: {
+      subql: 'https://api.subquery.network/sq/darwinia-network/pangolin',
     },
-    fullName: 'Pangolin Testnet',
-    ss58Prefix: 18,
     dvmWithdrawAddress: {
-      ring: '0x0000000000000000000000000000000000000015',
       kton: '0x0000000000000000000000000000000000000015',
+      ring: '0x0000000000000000000000000000000000000015',
     },
-    token: { ring: 'PRING', kton: 'PKTON' },
     erc20: {
-      ring: '0xbBD91aD844557ADCbb97296216b3B3c977FCC4F2',
       kton: '0xc8C1680B18D432732D07c044669915726fAF67D0',
+      ring: '0xbBD91aD844557ADCbb97296216b3B3c977FCC4F2',
     },
-    rpc: 'wss://pangolin-rpc.darwinia.network/',
     ethereumChain: {
+      blockExplorerUrls: ['https://pangolin.subscan.io/'],
       chainId: '0x2b',
       chainName: 'pangolin',
       iconUrls: [],
       nativeCurrency: {
+        decimals: 18,
         name: 'Pangolin Network Native Token',
         symbol: 'PRING',
-        decimals: 18,
       },
       rpcUrls: ['https://pangolin-rpc.darwinia.network/'],
-      blockExplorerUrls: ['https://pangolin.subscan.io/'],
     },
-    api: {
-      subql: 'https://api.subquery.network/sq/darwinia-network/pangolin',
-    },
-  },
-  crab: {
     facade: {
-      logo: '/image/crab.svg',
-      logoWithText: '/image/crab-logo.svg',
+      logo: '/image/pangolin.svg',
+      logoWithText: '/image/pangolin-logo.svg',
     },
-    fullName: 'Crab Mainnet',
-    ss58Prefix: 42,
-    dvmWithdrawAddress: {
-      ring: '0x0000000000000000000000000000000000000015',
-      kton: '0x0000000000000000000000000000000000000016',
-    },
-    token: { ring: 'CRAB', kton: 'CKTON' },
-    erc20: {
-      ring: '0x588abe3F7EE935137102C5e2B8042788935f4CB0',
-      kton: '0xbfE9E136270cE46A2A6a8E8D54718BdAEBEbaA3D',
-    },
-    rpc: 'wss://crab-rpc.darwinia.network',
-    ethereumChain: {
-      chainId: '0x2c',
-      chainName: 'crab',
-      iconUrls: [],
-      nativeCurrency: {
-        name: 'Crab Network Native Token',
-        symbol: 'CRAB',
-        decimals: 18,
-      },
-      rpcUrls: ['https://crab-rpc.darwinia.network/'],
-      blockExplorerUrls: ['https://crab.subscan.io/'],
-    },
-    api: {
-      subql: 'https://api.subquery.network/sq/darwinia-network/crab',
+    fullName: 'Pangolin',
+    rpc: 'wss://pangolin-rpc.darwinia.network/',
+    ss58Prefix: 18,
+    token: {
+      kton: 'PKTON',
+      ring: 'PRING',
     },
   },
 };
